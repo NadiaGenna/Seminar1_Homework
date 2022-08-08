371349840af4dd7836a72a253f6ca9da88be50ae
nkolarzh@gmail.com <Kolarzh_Nadezhda>
2022-08-08 03:01:43 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7->max = 7
+//a = 2 b = 10->max = 10
+//a = -9 b = -3->max = -3
+
+
+var n1 = Decimal.Parse(Console.ReadLine());
+         var n2 = Decimal.Parse(Console.ReadLine());
+         if (n1 > n2)
+             Console.WriteLine(n1);
+         else
+             Console.WriteLine(n2);
