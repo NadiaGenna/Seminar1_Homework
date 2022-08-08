f005279efab54a53119e897a44a5d1f76df7a494
nkolarzh@gmail.com <Kolarzh_Nadezhda>
2022-08-08 04:14:15 +0300

    Correct Task2 and Task4

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,4 @@
-﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿//  Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 //a = 5; b = 7->max = 7
 //a = 2 b = 10->max = 10
 //a = -9 b = -3->max = -3
@@ -16,3 +16,20 @@ int num_B = Convert.ToInt32(Console.ReadLine());
     {
          Console.WriteLine("Второе число " + num_B + " больше, чем первое " + num_A);
     }
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числа:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int num_2 = Convert.ToInt32(Console.ReadLine());
+int num_3 = Convert.ToInt32(Console.ReadLine());
+int max = num_1;
+if (num_2 > max)
+{
+    max = num_2;
+}
+if (num_3 > max)
+{
+    max = num_3;
+}
+Console.WriteLine("Наибольшее из введенных чисел: " + max);
+
